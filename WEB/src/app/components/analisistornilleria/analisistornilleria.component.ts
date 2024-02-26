7b8269a4591468ea76fc95496a0ea8281a831b90
Alex Ri <61464820+MasterAlex7@users.noreply.github.com>
2024-02-26 11:18:13 -0600

    Maquetacion Analisis

--- WEB/src/app/components/analisistornilleria/analisistornilleria.component.ts
+++ WEB/src/app/components/analisistornilleria/analisistornilleria.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-analisistornilleria',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule
+  ],
   templateUrl: './analisistornilleria.component.html',
   styleUrl: './analisistornilleria.component.css'
 })
